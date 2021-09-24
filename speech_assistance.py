5a55531526f3d6eeebccdc2cfc45bc5edcfa79c7
ShahabAthar25 <shahabathar25@gmail.com>
2021-09-24 23:29:36 +0500

    Add Machine Learning Model

--- speech_assistance.py
+++ speech_assistance.py
@@ -1,3 +1,4 @@
+from typing import Mapping
 from neuralintents import GenericAssistant
 import speech_recognition as sr
 import pyttsx3 as tts
@@ -7,3 +8,15 @@ r = sr.Recognizer()
 
 speaker = tts.init()
 speaker.setProperty('rate', 150)
+
+todo_list = ['Go Shopping', 'Clean Room']
+
+def print_hello():
+    print("Hello")
+
+mappings = {'greeting': print_hello}
+
+assistance = GenericAssistant('intents.json', intent_methods=mappings)
+assistance.train_model()
+
+assistance.request("How are you")
